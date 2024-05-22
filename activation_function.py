96cbe3bcca771db10dc16d9867d40f6d2c63d73a
Sapik <2002jakubs@gmail.com>
2024-05-22 23:55:03 +0200

    Added softmax

--- activation_function.py
+++ activation_function.py
@@ -5,24 +5,6 @@ from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-X, y = spiral_data(100, 3)
-
-def create_data(points, classes):
-    X = np.zeros((points*classes, 2))
-    y = np.zeros(points*classes, dtype='uint8')
-    for class_number in range(classes):
-        ix = range(points*class_number, points*(class_number+1))
-        r = np.linspace(0.0, 1, points) # radius
-        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
-        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
-        y[ix] = class_number
-    return X, y
-
-class Activation_ReLu:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons);
@@ -30,9 +12,31 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(2, 5)
+
+class Activation_ReLu:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # axis=1 -> we sum the values in the row
+        # keepdims=True -> keep the dimensions of the array, no need for transpose
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLu()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3,3)   # output of first=3, input of second=3
+activation2 = Activation_Softmax()  # softmax because its the last layer
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:10])
